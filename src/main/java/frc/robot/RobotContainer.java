144ab8da009e6033ebdb546f6dba4ae83221fce1
Liam <mountnRocky@gmail.com>
2025-10-27 20:40:52 -0600

    Semi-working code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,12 +12,14 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.PivotSubsystem;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -31,7 +33,7 @@ public class RobotContainer {
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
-
+    private final PivotSubsystem m_PivotSubsystem = new PivotSubsystem();
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
@@ -58,7 +60,8 @@ public class RobotContainer {
         RobotModeTriggers.disabled().whileTrue(
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
-
+        joystick.x().onTrue(new InstantCommand(m_PivotSubsystem::upDeg));
+        joystick.y().onTrue(new InstantCommand(m_PivotSubsystem::downDeg));
         joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
         joystick.b().whileTrue(drivetrain.applyRequest(() ->
             point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
---
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -0,0 +1,131 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import java.nio.channels.NotYetBoundException;
+
+import com.ctre.phoenix6.*;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class PivotSubsystem extends SubsystemBase{
+
+    TalonFX FrontLeft;
+    TalonFX FrontRight;
+    TalonFX RearRight;
+    TalonFX RearLeft;
+
+    CANcoder encooder;
+
+    boolean LeftInveted;
+    boolean RightInverted;
+    double angle;
+
+    public double setPoint;
+    
+    public enum Dir{
+        UP,
+        DOWN
+    }
+
+    public PivotSubsystem(){
+        FrontLeft = new TalonFX((int)Constants.PivotConstants.FrontLeftPivotMotorID, "canivore");
+        FrontRight = new TalonFX((int)Constants.PivotConstants.FrontRightPivotMotorID, "canivore");
+        RearLeft = new TalonFX((int)Constants.PivotConstants.RearLeftPivotMotorID, "canivore");
+        RearRight = new TalonFX((int)Constants.PivotConstants.RearRightPivotMotorID, "canivore");
+
+        LeftInveted = Constants.PivotConstants.leftInverted;
+        RightInverted = Constants.PivotConstants.rightInverted;
+
+        encooder = new CANcoder((int)Constants.PivotConstants.EncooderID, "canivore");
+
+        angle = encooder.getAbsolutePosition().getValueAsDouble();
+        setPoint = angle;
+    }
+
+    public void moveUp(){
+        moveMotors(Dir.UP, Constants.PivotConstants.maxSpeed);
+    }
+
+    public void moveDown(){
+        moveMotors(Dir.DOWN, Constants.PivotConstants.maxSpeed);
+    }
+
+    public void moveMotors(Dir dir, double speed){
+        if (dir == Dir.UP){
+            if (LeftInveted && !RightInverted){
+                FrontLeft.set(-speed);
+                RearLeft.set(-speed);
+
+                FrontRight.set(speed);
+                RearRight.set(speed);
+            }
+            else if (RightInverted && !LeftInveted){
+                FrontLeft.set(speed);
+                RearLeft.set(speed);
+
+                FrontRight.set(-speed);
+                RearRight.set(-speed);
+            }
+            else {
+                throw new NotYetBoundException();
+            }
+        } else if (dir == Dir.DOWN) {
+            if (LeftInveted && !RightInverted){
+                FrontLeft.set(speed);
+                RearLeft.set(speed);
+
+                FrontRight.set(-speed);
+                RearRight.set(-speed);
+            }
+            else if (RightInverted && !LeftInveted){
+                FrontLeft.set(-speed);
+                RearLeft.set(-speed);
+
+                FrontRight.set(speed);
+                RearRight.set(speed);
+            }
+            else {
+                throw new NotYetBoundException();
+            }
+        } else {
+            throw new NotYetBoundException();
+        }
+    }
+
+    public void stopMotors(){
+        FrontLeft.set(0);
+        FrontRight.set(0);
+        RearLeft.set(0);
+        RearRight.set(0);
+    }
+
+    public void setSetpoint(double angle) {
+        setPoint = angle;
+    }
+
+    public void upDeg() {
+        setPoint+= 0.01;
+    }
+
+    public void downDeg() {
+        setPoint-= 0.01;
+    }
+
+    @Override
+    public void periodic() {
+        angle = encooder.getAbsolutePosition().getValueAsDouble();
+
+        if (angle == setPoint) {
+            stopMotors();
+        } else if (angle >= setPoint) {
+            moveDown();
+        } else if (angle <= setPoint){
+            moveUp();
+        }
+
+        System.out.println("===========" + "ENCOOER VALUE: " + angle + " | SETPOINT: " + setPoint + "===========");
+    }
+}
