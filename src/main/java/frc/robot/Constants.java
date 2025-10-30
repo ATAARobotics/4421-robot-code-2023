63cc144b393f76462539caad212d0e6af30ebb13
Liam <mountnRocky@gmail.com>
2025-10-29 18:21:17 -0600

    Max speed lowered

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+public class Constants {
+    public static class PivotConstants {
+        public static final double FrontLeftPivotMotorID = 31;
+        public static final double RearLeftPivotMotorID = 33;
+        public static final double FrontRightPivotMotorID = 32;
+        public static final double RearRightPivotMotorID = 34;
+
+        public static final double EncooderID = 35;
+
+        public static final double maxSpeed = 0.15;
+        public static final double tolerance = 0.005;
+
+        public static final double maxAngle = 0.241455;
+        public static final double minAngle = 0.01;
+
+        public static final boolean leftInverted = true;
+        public static final boolean rightInverted = false;
+
+    }    
+}
